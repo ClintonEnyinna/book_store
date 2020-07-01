40791706177bbabc8360ae73bf32efda926904c1
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-07-01 04:01:23 -0500

    include dependent destroy in has many associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_one :order
+  has_one :order, dependent: :destroy
   has_one :buyer, through: :order
   belongs_to :seller
 end
--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,4 +1,4 @@
 class Buyer < ApplicationRecord
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_many :books, through: :orders
 end
