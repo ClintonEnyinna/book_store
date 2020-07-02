2cdb467bc0395ae52cc860a3f26cf6643956872f
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-07-01 19:00:07 -0500

    feat: use has_secure_password for authentication

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -4,4 +4,7 @@ class Buyer < ApplicationRecord
 
   validates :name, :email, :address, presence: true
   validates :balance, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,4 +2,8 @@ class Seller < ApplicationRecord
   has_many :registered_books, foreign_key: 'seller_id', class_name: 'Book', dependent: :destroy
 
   validates :name, :email, :phone, presence: true
+  validates :earnings, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
